d9c7f63b355629064e3ede26715095d36765b804
Ulia Lukash <ulianalukash@yandex.ru>
2024-03-05 10:31:03 +0300

    profile service

--- FakeNFT/Services/ProfileService.swift
+++ FakeNFT/Services/ProfileService.swift
@@ -11,26 +11,28 @@ typealias ProfileCompletion = (Result<Profile, Error>) -> Void
 
 protocol ProfileService {
     func loadProfile(id: String, completion: @escaping ProfileCompletion)
-    func updateProfile(dto: String,
-                       id: String,
-                       completion:  @escaping ProfileCompletion)
+    func loadProfile(completion: @escaping ProfileCompletion)
+    func updateProfile(dto: String, id: String,
+                       completion: @escaping ProfileCompletion)
+    func updateProfile(likes: [String], completion: @escaping ProfileCompletion)
 }
 
-//MARK: - ProfileServiceImpl
+// MARK: - ProfileServiceImpl
 final class ProfileServiceImpl {
     private let networkClient: NetworkClient
     private let storage: ProfileStorageProtocol
-    
+
     init(networkClient: NetworkClient, storage: ProfileStorageProtocol) {
         self.storage = storage
         self.networkClient = networkClient
     }
 }
 
-//MARK: - ProfileService
+// MARK: - ProfileService
 extension ProfileServiceImpl: ProfileService {
+
     func loadProfile(id: String, completion: @escaping ProfileCompletion) {
-        if let profile = storage.getProfile(with: id) {
+        if let profile = storage.getProfile() {
             completion(.success(profile))
             return
         }
@@ -45,18 +47,59 @@ extension ProfileServiceImpl: ProfileService {
             }
         }
     }
-    
+    func loadProfile(completion: @escaping ProfileCompletion) {
+        if let profile = storage.getProfile() {
+            completion(.success(profile))
+            return
+        }
+        let request = ProfileRequest()
+        networkClient.send(request: request,
+                           type: Profile.self) {result in
+            switch result {
+            case .success(let profile):
+                completion(.success(profile))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
     func updateProfile(dto: String, id: String, completion: @escaping ProfileCompletion) {
         let request = ProfilePutRequest(dto: dto)
         networkClient.send(request: request,
                            type: Profile.self) { [weak self, storage] result in
             guard let self else { return }
-            storage.removeProfile(with: id)
+            storage.removeProfile()
             switch result {
-            case .success(_):
-                self.loadProfile(id: id) { result in
+            case .success:
+                self.loadProfile { result in
                     switch result {
                     case .success(let profile):
+                        completion(.success(profile))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func updateProfile(likes: [String], completion: @escaping ProfileCompletion) {
+
+        let dto = likes.map {"likes=\($0)"}.joined(separator: "&")
+        let request = ProfilePutRequest(dto: dto)
+        networkClient.send(request: request,
+                           type: Profile.self) { [weak self, storage] result in
+            guard let self else { return }
+            storage.removeProfile()
+            switch result {
+            case .success:
+                self.loadProfile { result in
+                    switch result {
+                    case .success(let profile):
+                        storage.saveProfile(profile: profile)
                         completion(.success(profile))
                     case .failure(let error):
                         completion(.failure(error))
