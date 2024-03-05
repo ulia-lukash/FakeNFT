73a8cee52a834a1787703ad258f072cca7e3635c
Ulia Lukash <ulianalukash@yandex.ru>
2024-03-05 15:10:24 +0300

    wip

--- FakeNFT/Services/Requests/GETOrderRequest.swift
+++ FakeNFT/Services/Requests/GETOrderRequest.swift
@@ -5,15 +5,3 @@ struct GetOrderRequest: NetworkRequest {
         URL(string: "\(RequestConstants.baseURL)/api/v1/orders/1")
     }
 }
-
-struct PutOrderRequest: NetworkRequest {
-    var nfts: [String]
-    var httpMethod: HttpMethod {.put}
-    var endpoint: URL? {
-        URL(string: "\(RequestConstants.baseURL)/api/v1/orders/1")
-    }
-    var dto: Encodable? {
-        let nftString = nfts.map { "nfts=\($0)" }.joined(separator: "&")
-        return nftString
-    }
-}
--- FakeNFT/Services/Requests/PUTOrderRequest.swift
+++ FakeNFT/Services/Requests/PUTOrderRequest.swift
@@ -1,15 +1,20 @@
+//
+//  PUTOrderRequest.swift
+//  FakeNFT
+//
+//  Created by Uliana Lukash on 05.03.2024.
+//
+
 import Foundation
 
 struct PutOrderRequest: NetworkRequest {
-  // MARK: - Properties:
-  var httpMethod: HttpMethod = .put
-  var dto: Encodable?
-  var endpoint: URL? {
-    URL(string: "\(RequestConstants.baseURL)/api/v1/orders/1")
-  }
-  // MARK: - Methods:
-  init(nfts: [String]) {
-    let queryString = nfts.map { "nfts=\($0)" }.joined(separator: "&")
-    self.dto = queryString
-  }
+    var nfts: [String]
+    var httpMethod: HttpMethod {.put}
+    var endpoint: URL? {
+        URL(string: "\(RequestConstants.baseURL)/api/v1/orders/1")
+    }
+    var dto: Encodable? {
+        let nftString = nfts.map { "nfts=\($0)" }.joined(separator: "&")
+        return nftString
+    }
 }
